b82cd294e408a125f6cb403d5e5961e9fa0d5d04
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-10 10:38:55 -0700

    Updated javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Jonathan Tran';
+const isCool = false;
+const totalPets = 0;
+const unknown = null;
+let never;
+
+console.log('value of fullName: ' + fullName);
+console.log('value of isCool: ' + isCool);
+console.log('value of totalPets: ' + totalPets);
+
+console.log('type of fullName: ' + typeof (fullName));
+console.log('type of isCool: ' + typeof (isCool));
+console.log('type of totalPets: ' + typeof (totalPets));
+
+console.log('value of unknown: ' + unknown);
+console.log('type of unknown: ' + typeof (unknown));
+
+console.log('value of never: ' + never);
+console.log('type of never: ' + never);
