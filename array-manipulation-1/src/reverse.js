f126a16b2c5edf821bf542b73fec407beaace717
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-13 16:33:54 -0700

    Updated.

---
+++ array-manipulation-1/src/reverse.js
@@ -0,0 +1,8 @@
+/* exported reverse */
+function reverse(array) {
+  var reversedArr = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    reversedArr.push(array[i]);
+  }
+  return reversedArr;
+}
