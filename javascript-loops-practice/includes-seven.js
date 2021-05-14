d0caaa997cf5902b75a27ef68fe587b6e83648f9
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-13 19:49:14 -0700

    Different way javascript loops-practice.

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,14 +1,23 @@
 /* exported includesSeven */
+// function includesSeven(array) {
+//   var onlySeven = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] === 7) {
+//       onlySeven.push(array[i]);
+//     }
+//   }
+//   if (onlySeven.length >= 1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
 function includesSeven(array) {
-  var onlySeven = [];
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 0; i <= array.length - 1; i++) {
     if (array[i] === 7) {
-      onlySeven.push(array[i]);
+      return true;
     }
   }
-  if (onlySeven.length >= 1) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
