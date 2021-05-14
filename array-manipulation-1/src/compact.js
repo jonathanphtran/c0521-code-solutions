84cb56126c638a0e94e7de25559591573c3b35b0
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-13 19:36:19 -0700

    Complete array manipulation 1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,7 +2,7 @@
 function compact(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === false) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
