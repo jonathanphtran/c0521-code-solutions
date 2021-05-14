8f3df6fa4c26bbb99b270bab1442124b07136455
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-14 10:01:31 -0700

    Updated string-manipulation-1.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,51 +1,18 @@
 /* exported getWords */
 function getWords(string) {
   var words = [];
-  var currentIdx = 0;
-  var i = 0;
-  var j = 0;
-  var oneWord = '';
-  var finalWord = '';
+  var currentWord = '';
 
-  // debugger;
+  if (string.length === 0) return words;
 
-  if (string === '') {
-    return words;
-  }
-
-  var spaces = [];
-  for (var h = 0; h < string.length; h++) {
-    if (string[h] === ' ') {
-      spaces.push(string[h]);
-      break;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      currentWord += string[i];
+    } else {
+      words.push(currentWord);
+      currentWord = '';
     }
   }
-  if (spaces.length === 0) {
-    for (var z = 0; z < string.length; z++) {
-      oneWord += string[z];
-    }
-    words.push(oneWord);
-    return words;
-  }
-
-  while (i < string.length) {
-    if (string[i] === ' ') {
-      var word = '';
-      while (currentIdx < i) {
-        if (string[currentIdx] !== ' ') {
-          word += string[currentIdx];
-        }
-        currentIdx++;
-      }
-      words.push(word);
-    }
-    i++;
-  }
-
-  for (j = currentIdx + 1; j < string.length; j++) {
-    finalWord += string[j];
-  }
-  words.push(finalWord);
-
+  words.push(currentWord);
   return words;
 }
