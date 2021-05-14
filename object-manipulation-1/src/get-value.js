207f9eba9d31235d30b2a02411f19df1481fdc6d
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-13 19:30:23 -0700

    Completed Object-Manipulation-1.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,8 @@
+/* exported getValue */
+function getValue(object, key) {
+  return object[key];
+}
+
+// const dave = { firstName: 'David', lastName: 'Thomas' };
+// var x = getValue(dave, 'firstName'); // -> "David"
+// var y = getValue(dave, 'lastName'); // -> "Thomas"
