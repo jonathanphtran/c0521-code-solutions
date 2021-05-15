37c7a3c0750fd700fd9e8c937538323080f4567a
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-15 15:37:51 -0700

    Updated object-manipulation-2.

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,30 @@
+/* exported pick */
+
+/*
+
+***************************** PSEUDOCODE *****************************
+
+- create new object container
+- look through each property in the object
+- compare each property the the values in the array
+- if they are equal, add the key value pair to the new object
+- give back the new object
+
+**********************************************************************
+
+*/
+function pick(source, key) {
+  var newObj = {};
+  for (var akey in source) {
+    for (var i = 0; i < key.length; i++) {
+      if (source[akey] === undefined) {
+        break;
+      }
+      if (akey === key[i]) {
+        newObj[akey] = source[akey];
+        break;
+      }
+    }
+  }
+  return newObj;
+}
