75eecc4c7c5c62c78f23668b562296d3bce3107b
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-15 14:53:33 -0700

    Completed array manipulation.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -6,11 +6,15 @@
 
 - if the size is greater than array length, give back an empty array
 - create an array container
+- create a variable to check size of array
 - look through the whole array
+- create an array for each chunk
 - create an inner loop that looks through the size
-- add elements to a new array
-- update i by adding size to it and subtracting 1
-- if there is an overflow, then
+- add elements to the chunk array
+- subtract 1 from the array length counter
+- if the array length counter is 0, break the loop
+  - this is to stop adding empty items to the array
+- set i so to the index of the first item that hasn't been added yet
 - give back the array container
 
 **************************************************************************
@@ -20,17 +24,18 @@
 function chunk(array, size) {
   if (size > array.length) return [];
   var arrayContainer = [];
+  var arrayLengthCounter = array.length;
   for (var i = 0; i < array.length; i++) {
     var chunks = [];
     for (var j = 0; j < size; j++) {
       chunks.push(array[j + i]);
+      arrayLengthCounter--;
+      if (arrayLengthCounter === 0) {
+        break;
+      }
     }
     arrayContainer.push(chunks);
     i = i + size - 1;
   }
-  // console.log(arrayContainer);
   return arrayContainer;
 }
-
-var arr = [false, true, false, true];
-chunk(arr, 3);
