7f98a7978afccfe8d6cbafc057b6deb2f9ad4430
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-15 19:28:17 -0700

    Updated.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,16 @@
 /* exported defaults */
+
+/*
+
+***************************** PSEUDOCODE *****************************
+
+- create a counter
+- look through each property of the source and compare it to the target
+- if there is a match, add one to the counter
+- if the counter is equivalent to 0, add the property to the target
+- reset the counter
+- return the target object
+
+**********************************************************************
+
+*/
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -14,6 +14,7 @@
 **********************************************************************
 
 */
+
 function omit(source, key) {
   var newObj = {};
   for (var akey in source) {
