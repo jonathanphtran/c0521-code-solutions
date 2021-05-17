f01eb4eea865e8a18d7a21f78117bd97b3f84717
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-17 11:12:02 -0700

    Updated dom-querying.

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -4,9 +4,13 @@ var $heading = document.querySelector('h1');
 console.log('$heading: ', $heading);
 console.dir($heading);
 
-var $explanation = document.querySelector('.hint');
+var $explanation = document.querySelector('#explanation');
 console.log('$explanation', $explanation);
 console.dir($explanation);
+
+var $hint = document.querySelectorAll('.hint');
+console.log('$hint', $hint);
+console.dir($hint);
 
 var $paragraphs = document.querySelectorAll('p');
 console.log('$paragraph: ', $paragraphs);
