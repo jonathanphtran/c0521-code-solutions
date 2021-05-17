fad7a6fd46a1bbc69a693cb1e5e27364238745e2
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-17 11:04:19 -0700

    Updated String-manipulation-3.

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,26 @@
+/* exported isAnagram */
+
+/*
+
+*************************** PSEUDOCODE ***********************************
+
+- create an empty object
+- create another empty object
+- look through each character of the first string
+- if you see a letter for the first time, add it as a property to
+  - the object
+  - set value to 1
+- if it isn't new, increase value by 1
+- do the same with the next string
+- look through each property of the first object and compare it with
+  - the second object
+- if every property and value match, give back true
+  - if not, give back false
+
+**************************************************************************
+
+*/
+
+// function isAnagram(firstString, secondString) {
+
+// }
