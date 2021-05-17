02bb6ce8fc7e8895fdb4889c6c046f06c0c4c703
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-17 13:30:28 -0700

    string-manipulation-3

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,18 @@
 /* exported reverseWords */
+
+/*
+
+*************************** PSEUDOCODE ***********************************
+
+- create a string container
+- create a temporary variable that could hold your position in the string
+- create another temprary variable that holds the amount of spaces
+  - you have moved up since the last space
+- look through each character in the string
+- if the character is a space
+  - set the temporary variable to the index
+  -
+
+**************************************************************************
+
+*/
