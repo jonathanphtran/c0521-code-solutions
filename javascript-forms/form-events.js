3848ceffeb598d9957ce63d3b2e00662f3601849
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-19 11:20:35 -0700

    Updated javascript-forms.

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -27,19 +27,3 @@ $userEmail.addEventListener('input', handleInput);
 $userMessage.addEventListener('focus', handleFocus);
 $userMessage.addEventListener('blur', handleBlur);
 $userMessage.addEventListener('input', handleInput);
-
-var $contactForm = document.querySelector('#contact-form');
-
-function storeInObj(event) {
-  event.preventDefault();
-  var valueObj = {};
-
-  for (var i = 0; i < document.forms[0].length - 1; i++) {
-    valueObj[document.forms[0].elements[i].name] = document.forms[0].elements[i].value;
-  }
-
-  console.log(valueObj);
-  $contactForm.reset();
-}
-
-$contactForm.addEventListener('submit', storeInObj);
