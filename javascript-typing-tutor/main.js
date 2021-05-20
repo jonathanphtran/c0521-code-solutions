80b00da8a426d7854bb9525700dc21ed51046e9b
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-20 10:43:52 -0700

    Updated javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,26 +2,30 @@ var $words = document.querySelector('#sentence');
 var $submit = document.querySelector('.submit');
 var $middle = document.querySelector('.middle');
 var $HTML = document.querySelector('html');
-var $newEl = document.querySelectorAll('h1');
+var $newEl = document.querySelectorAll('span');
 
 var i = 0;
 
 function getSentence(event) {
   event.preventDefault();
   var words = $words.value;
+  var newEl = document.createElement('h1');
 
   for (var j = 0; j < words.length; j++) {
     if (words[j] !== ' ') {
-      var newEl = document.createElement('h1');
-      newEl.append(words[j]);
-      $middle.append(newEl);
+      var newSpanEl = document.createElement('span');
+      newSpanEl.append(words[j]);
+      newEl.append(newSpanEl);
     } else {
-      newEl = document.createElement('h1');
-      newEl.innerHTML = '&nbsp';
-      $middle.append(newEl);
+      newSpanEl = document.createElement('span');
+      newSpanEl.innerHTML = ' ';
+      newEl.append(newSpanEl);
     }
   }
-  $newEl = document.querySelectorAll('h1');
+
+  $middle.append(newEl);
+
+  $newEl = document.querySelectorAll('span');
 
   $words.value = '';
 
@@ -33,11 +37,11 @@ function getLetter(event) {
   event.preventDefault();
   var letter = String.fromCharCode(event.keyCode);
 
-  if (i === $newEl.length - 1) {
+  if (i === $newEl.length - 1 && letter === $newEl[i].textContent) {
     $newEl[i].className = 'green';
     return 'FINISHED';
   }
-  if ((letter === $newEl[i].textContent) || (letter === ' ' && $newEl[i].innerHTML === '&nbsp;')) {
+  if ((letter === $newEl[i].textContent) || (letter === ' ' && $newEl[i].innerHTML === ' ')) {
     $newEl[i].className = 'green';
     $newEl[i + 1].className = 'currentLetter';
     i++;
