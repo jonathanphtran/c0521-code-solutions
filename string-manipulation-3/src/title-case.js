68f13994f0ede59f048316a2571952d4130f60a1
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-27 17:40:51 -0700

    Fixed order of conditionals.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,68 +1,70 @@
-// /* exported titleCase */
+// // /* exported titleCase */
 
-// /*
+// // /*
 
-// *************************** PSEUDOCODE ***********************************
-// - create a new string container
-// - lowercase string into a new string
-// - create 3 arrays, consisting of conjunctions, articles, and prepositions
-// - create a counter
-// - create a temporary word variable
-// - everytime you move up a letter, increase the counter
-// - stop when you reach a space
-// - check if the counter is above three
-//   - if not, check if it matches a word in one of the three arrays
-//   - then check if the previosu word is a colon
-//   - if it matches, and a colon doesn't precede it, keep it lower-cased,
-//     - otherwise capitalize the first letter
-// - check if the character is a dash
-// - reset the counter
-// - once you reach the end, capitalize the last word and add it in
-// **************************************************************************
+// // *************************** PSEUDOCODE ***********************************
+// // - create a new string container
+// // - lowercase string into a new string
+// // - create 3 arrays, consisting of conjunctions, articles, and prepositions
+// // - create a counter
+// // - create a temporary word variable
+// // - everytime you move up a letter, increase the counter
+// // - stop when you reach a space
+// // - check if the counter is above three
+// //   - if not, check if it matches a word in one of the three arrays
+// //   - then check if the previosu word is a colon
+// //   - if it matches, and a colon doesn't precede it, keep it lower-cased,
+// //     - otherwise capitalize the first letter
+// // - check if the character is a dash
+// // - reset the counter
+// // - once you reach the end, capitalize the last word and add it in
+// // **************************************************************************
 
-// */
+// // */
 
 // function titleCase(string) {
 //   var title = '';
 //   var loweredStr = string.toLowerCase();
 //   var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at',
 //     'by', 'for', 'in', 'of', 'on', 'per', 'to'];
-//   var tempStr = '';
-//   var count = 1;
 //   var words = [];
-//   var tempWord = '';
-
+//   var currentWord = '';
+//   var k = 0;
 //   loweredStr = loweredStr.replace('javascript', 'JavaScript');
 //   loweredStr = loweredStr.replace('api', 'API');
-
 //   title += loweredStr[0].toUpperCase();
 
-//   for (var z = 0; z < loweredStr.length; z++) {
-//     if (tempWord.length > 3 && loweredStr[z] !== ' ') {
-//       z++;
-//     } else if (loweredStr[z] === ' ' && tempWord.length < 4) {
-//       words.push(tempWord);
-//       tempWord = '';
-//     } else {
-//       tempWord += loweredStr[z];
-//       console.log(tempWord);
-//     }
+//   if (loweredStr.length === 0) return words;
 
-//   }
-//   console.log(words);
+//   // for (var j = 0; j < loweredStr.length; j++) {
+//   //   if (string[j] !== ' ') {
+//   //     currentWord += loweredStr[j];
+//   //   } else {
+//   //     words.push(currentWord);
+//   //     currentWord = '';
+//   //   }
+//   // }
+//   // words.push(currentWord);
+//   // console.log(words);
 
 //   for (var i = 1; i < loweredStr.length; i++) {
-//     if (loweredStr[i] !== ' ' && loweredStr[i] !== '-') {
-//       title += loweredStr[i];
-//     } else if (loweredStr[i] === ' ') {
-//       title += ' ';
-//       title += loweredStr[i + 1].toUpperCase();
-//       i++;
+
+//     if (loweredStr[i] === ':') {
+//       title += ': ';
+//       title += loweredStr[i + 2].toUpperCase();
+//       i += 2;
 //     } else if (loweredStr[i] === '-') {
 //       title += '-';
 //       title += loweredStr[i + 1].toUpperCase();
 //       i++;
+//     } else if (loweredStr[i] !== ' ') {
+//       title += loweredStr[i];
+//     } else {
+//       title += ' ';
+//       title += loweredStr[i + 1].toUpperCase();
+//       i++;
 //     }
+
 //   }
 //   return title;
 // }
